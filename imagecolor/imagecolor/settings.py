ac84271b0860b03fd2d7122cce3820862d6d0173
Ajay Pal <ajay.pec24@gmail.com>
2022-07-13 17:29:47 +0530

    1

--- imagecolor/imagecolor/settings.py
+++ imagecolor/imagecolor/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Custom Variable for templates etc
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
