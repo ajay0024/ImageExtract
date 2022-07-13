fe6553f63e838fceb4475357b476c0747f25009b
Ajay Pal <ajay.pec24@gmail.com>
2022-07-13 14:46:17 +0530

    Added STATIC_ROOT as required by heroku

--- imagecolor/imagecolor/settings.py
+++ imagecolor/imagecolor/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Custom Variable for templates etc
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
