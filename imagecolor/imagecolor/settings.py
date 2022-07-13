4b0b171ca1cb19c2067ed346f9ad8d5ff4dc811b
Ajay Pal <ajay.pec24@gmail.com>
2022-07-13 18:14:42 +0530

    11

--- imagecolor/imagecolor/settings.py
+++ imagecolor/imagecolor/settings.py
@@ -131,5 +131,5 @@ WHITENOISE_USE_FINDERS = True
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
